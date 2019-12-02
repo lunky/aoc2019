471ccecf1417d1bbce47bf9fd55d314632dbbe9c
Quinn Wilson <github@midworld.org>
2019-12-02 14:15:31 -0600

    day 2 working

--- src/Day2.hs
+++ src/Day2.hs
@@ -2,11 +2,33 @@ module Day2
     ( 
         day2
        ,day2b
+       ,intCode
     ) where
+import Data.List.Split (splitOn)
 
+input="1,9,10,3,2,3,11,0,99,30,40,50"
 
 day2 :: String -> Int
-day2 input = 0
+day2 input = head $ intCode 0 $ seedInput 12 2 $ parseInput input
 
-day2b :: String -> Int
-day2b input = 0
+day2b input = length $ takeWhile (/=19690720) 
+        $ map (\(a,b) -> head $ intCode 0 (seedInput a b parsedInput)) [(x,y) | x<-[0..99], y<-[0..99]]
+  where parsedInput = parseInput input
+
+seedInput :: Int -> Int -> [Int] -> [Int]
+seedInput x y parsedInput = setVal (setVal parsedInput 1 x) 2 y
+
+intCode :: Int -> [Int] -> [Int]
+intCode offset inst 
+  | inst!! offset == 99 = inst
+  | inst!! offset == 1 = intCode (offset+4) (setVal inst dest (inst!!pos1 + inst!!pos2))
+  | inst!! offset == 2 = intCode (offset+4) (setVal inst dest (inst!!pos1 * inst!!pos2))
+  | otherwise = error ("invalid opcode " ++ show offset ++ " : " ++ show inst)
+    where [opCode,pos1,pos2,dest] = take 4 $ drop offset inst
+
+setVal :: [a] -> Int -> a -> [a]
+setVal lst idx val = begin ++ (val:end)
+  where (begin, _:end) = splitAt idx lst 
+
+parseInput :: String -> [Int]
+parseInput input = map read $ splitOn "," input
--- test/Day2Spec.hs
+++ test/Day2Spec.hs
@@ -8,3 +8,30 @@ spec = do
     describe "test harness" $ 
         it "should run a noop test" $ 
             1 `shouldBe` 1
+    describe "Day2" $ do
+        it "intCode should match pattern 1" $ do
+          let input = [1,9,10,3,2,3,11,0,99,30,40,50]
+          let expected = [3500,9,10,70,2,3,11,0,99,30,40,50]
+          intCode 0 input `shouldBe` expected
+        it "intCode should match pattern 2" $ do
+          let input = [1,9,10,70,2,3,11,0,99,30,40,50]
+          let expected = [3500,9,10,70,2,3,11,0,99,30,40,50]
+          intCode 4 input `shouldBe` expected
+
+        it "intCode should become pattern 1" $ do 
+          let input = [1,0,0,0,99] 
+          let expected = [2,0,0,0,99] --(1 + 1 = 2).
+          intCode 0 input `shouldBe` expected
+        it "intCode should become pattern 2" $ do 
+          let input = [2,3,0,3,99] 
+          let expected = [2,3,0,6,99] --(3 * 2 = 6).
+          intCode 0 input `shouldBe` expected
+        it "intCode should become pattern 3" $ do 
+          let input = [2,4,4,5,99,0] 
+          let expected = [2,4,4,5,99,9801] --(99 * 99 = 9801).
+          intCode 0 input `shouldBe` expected
+        it "intCode should become pattern 4" $ do 
+          let input = [1,1,1,4,99,5,6,0,99] 
+          let expected = [30,1,1,4,2,5,6,0,99]
+          intCode 0 input `shouldBe` expected
+
