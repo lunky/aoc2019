358ae09432670d1796a2bc182d5549123129eb9a
Quinn Wilson <github@midworld.org>
2019-12-29 20:30:35 -0600

    day8 working \o/

--- app/Main.hs
+++ app/Main.hs
@@ -9,6 +9,7 @@ import Day4
 import Day5
 import Day6
 import Day7
+import Day8
 
 -- timeStamp :: IO ()
 timeStamp f = do 
@@ -63,3 +64,9 @@ runDays = do
     let answer = show $ day7b contents
     putStrLn ("day7b: " ++ answer)
 
+    contents <- readFile "data/day8.txt"
+    let answer = show $ day8 (25,6) contents
+    putStrLn ("day8: " ++ answer)
+
+    let answer = day8b (25,6) contents
+    putStrLn ("day8b: \n" ++ answer)
---
+++ src/Day8.hs
@@ -0,0 +1,35 @@
+module Day8
+    ( 
+    day8
+   ,day8b
+    )
+    where
+import Data.Function (on)
+import Data.List (splitAt, minimumBy, transpose)
+import Data.List.Split (chunksOf)
+    
+day8 :: (Int,Int) -> String -> Int 
+day8 (width,height) input = (\y -> countLetters y '2' * countLetters y '1') 
+                    $ leastZeros 
+                    $ chunksOf (width*height) 
+                    $ filter (/='\n') input
+
+leastZeros = minimumBy (compare `on` zeros) 
+  where zeros input = countLetters input '0'
+
+countLetters :: String -> Char -> Int
+countLetters str c = length $ filter (==c) str
+
+day8b :: (Int,Int) -> String -> String
+day8b (width,height) input= unlines $ chunksOf width 
+                              $ day8b' input (width, height)
+
+helpShow '1' = '#'
+helpShow '0' = ' '
+helpShow x = x
+
+day8b' input (width,height) = map (helpShow.color) $ transpose $ chunksOf (width*height) $ filter (/='\n') input
+
+input = "0222112222120000"
+
+color = foldr (\y acc-> if y/='2' then y else acc) '2'
